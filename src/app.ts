709230be8041c6bc08366c21f38b0f15b932c46c
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-10 14:12:04 -0300

    core: add base login route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { handleError } from "./errors";
 import { questionsRoutes } from "./routes/questions.routes";
 import { answersRoutes } from "./routes/answers.routes";
 import { usersRoutes } from "./routes/users.routes";
+import { loginRoutes } from "./routes/login.routes";
 
 export const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use("/users", usersRoutes);
 app.use("/questions", questionsRoutes);
 app.use("/responses", answersRoutes);
+app.use("/login", loginRoutes);
 
 app.use(handleError);
