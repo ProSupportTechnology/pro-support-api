a36bbe43812a41594191ea63d91e228d945ed2f8
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-10 16:48:42 -0300

    fix: add relation to list question route

--- src/services/questions/list.service.ts
+++ src/services/questions/list.service.ts
@@ -2,6 +2,6 @@ import { AppDataSource } from "../../data-source";
 import { Question } from "../../entities/question.entity";
 
 export const listQuestionsService = async () => {
-  const questions = await AppDataSource.getRepository(Question).find();
+  const questions = await AppDataSource.getRepository(Question).find({ relations: { user: true } });
   return questions;
 };
