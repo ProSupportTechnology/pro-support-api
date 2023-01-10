a43987784ff0de062a880f08e3ac542b30ed842a
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-10 18:49:58 -0300

    fix: revert unecessary code

--- src/controllers/question.controllers.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { listQuestionsService } from "../services/questions/list.service";
-
-export const listQuestionsController = async (req: Request, res: Response) => {
-  const questions = await listQuestionsService();
-  return res.json(questions);
-};
--- src/services/questions/list.service.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from "../../data-source";
-import { Question } from "../../entities/question.entity";
-
-export const listQuestionsService = async () => {
-  const questions = await AppDataSource.getRepository(Question).find({ relations: { user: true } });
-  return questions;
-};
