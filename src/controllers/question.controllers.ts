7c0efa36ac24773231391453f7894c9842b440f2
Altieris Souza <altmsf15@gmail.com>
2023-01-10 17:47:40 -0300

    fix: creating routes to create and edit questions

--- src/controllers/question.controllers.ts
+++ src/controllers/question.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { iQuestionRequest } from "../interfaces/questions.interfaces";
 import { createQuestionService } from "../services/questions/create.service";
+import { editQuestionService } from "../services/questions/edit.service";
 
 export const createQuestionController = async (req: Request, res: Response) => {
   const questionData: iQuestionRequest = req.body;
@@ -8,4 +9,13 @@ export const createQuestionController = async (req: Request, res: Response) => {
   const newQuestion = await createQuestionService(questionData);
 
   return res.status(201).json(newQuestion);
+};
+
+export const editChangedController = async (req: Request, res: Response) => {
+  const changedData: iQuestionRequest = req.body;
+  const questionId = req.params.id;
+
+  const questionChanged = await editQuestionService(changedData, questionId);
+
+  return res.status(200).json(questionChanged);
 };
--- src/routes/questions.routes.ts
+++ src/routes/questions.routes.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
+import {
+  createQuestionController,
+  editChangedController,
+} from "../controllers/question.controllers";
 
 export const questionsRoutes = Router();
+
+questionsRoutes.post("", createQuestionController);
+
+questionsRoutes.patch("/:id", editChangedController);
