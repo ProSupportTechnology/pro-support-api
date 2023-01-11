26a18c3e4ee26f7435fcf01168ef7413b93a6593
Wesley de Matos Souza <wesleydematos3@gmail.com>
2023-01-11 08:49:23 -0300

    fix: change createAnswerService

---
+++ src/services/answers/create.service.ts
@@ -0,0 +1,58 @@
+import { AppDataSource } from "../../data-source";
+import { Answer } from "../../entities/answer.entity";
+import { Question } from "../../entities/question.entity";
+import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors";
+import {
+  iAnswerCreateResponse,
+  iAnswerRequest,
+} from "../../interfaces/answers.interfaces";
+import {
+  bodyAnswerSchema,
+  createAnswerResponseSchema,
+} from "../../schemas/answer.schemas";
+
+export const createAnswerService = async (
+  answerBody: iAnswerRequest
+): Promise<iAnswerCreateResponse> => {
+  const userId = answerBody.userId;
+  const questionId = answerBody.questionId;
+
+  const answersRepository = AppDataSource.getRepository(Answer);
+  const usersRepository = AppDataSource.getRepository(User);
+  const questionsRepository = AppDataSource.getRepository(Question);
+
+  const question = await questionsRepository
+    .findOneByOrFail({ id: questionId })
+    .catch(() => {
+      throw new AppError("Question not found", 404);
+    });
+
+  const user = await usersRepository
+    .findOneByOrFail({ id: userId })
+    .catch(() => {
+      throw new AppError("User not found", 404);
+    });
+
+  const answerValidated = await bodyAnswerSchema.validate(answerBody, {
+    stripUnknown: true,
+  });
+
+  const answer = answersRepository.create({
+    ...answerValidated,
+    user,
+    question,
+  });
+
+  await answersRepository.save(answer);
+
+  const response = await answersRepository.findOneBy({ id: answer.id });
+  const validatedResponse = await createAnswerResponseSchema.validate(
+    { ...response, userId, questionId },
+    {
+      stripUnknown: true,
+    }
+  );
+
+  return validatedResponse;
+};
