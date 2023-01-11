2e548a4dff9e887333f3361568280fd7e0b3df26
Wesley de Matos Souza <wesleydematos3@gmail.com>
2023-01-11 08:50:24 -0300

    feat: add createAnswerResponseSchema

--- src/schemas/answer.schemas.ts
+++ src/schemas/answer.schemas.ts
@@ -1,17 +1,36 @@
 import * as yup from "yup";
 import { SchemaOf } from "yup";
-import { iAnswerResponse, iAnswerRequest } from "../interfaces/answers.interfaces";
+import {
+  iAnswerResponse,
+  iAnswerRequest,
+  iAnswerCreateResponse,
+} from "../interfaces/answers.interfaces";
 
-export const answareResponseSchema: SchemaOf<iAnswerResponse> = yup.object().shape({
-  id: yup.string(),
-  description: yup.string(),
-  createdAt: yup.string(),
-  updatedAt: yup.string(),
-  user: yup.object(),
-  question: yup.object(),
-});
+export const answareResponseSchema: SchemaOf<iAnswerResponse> = yup
+  .object()
+  .shape({
+    id: yup.string(),
+    description: yup.string(),
+    createdAt: yup.string(),
+    updatedAt: yup.string(),
+    user: yup.object(),
+    question: yup.object(),
+  });
 
-export const listAnswareResponseSchema: SchemaOf<iAnswerResponse[]> = yup.array(answareResponseSchema);
+export const createAnswerResponseSchema: SchemaOf<iAnswerCreateResponse> = yup
+  .object()
+  .shape({
+    id: yup.string().required(),
+    description: yup.string().required(),
+    createdAt: yup.string().required(),
+    updatedAt: yup.string().required(),
+    userId: yup.string().required(),
+    questionId: yup.string().required(),
+  });
+
+export const listAnswareResponseSchema: SchemaOf<iAnswerResponse[]> = yup.array(
+  answareResponseSchema
+);
 
 export const bodyAnswerSchema: SchemaOf<iAnswerRequest> = yup.object().shape({
   description: yup.string().required(),
