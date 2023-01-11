4b37e61df146cd522359dd3c707aa97bf5b3137b
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-11 10:01:23 -0300

    fix: adjust validate schemas

--- src/services/answers/create.service.ts
+++ src/services/answers/create.service.ts
@@ -3,55 +3,35 @@ import { Answer } from "../../entities/answer.entity";
 import { Question } from "../../entities/question.entity";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors";
-import {
-  iAnswerCreateResponse,
-  iAnswerRequest,
-} from "../../interfaces/answers.interfaces";
-import {
-  bodyAnswerSchema,
-  createAnswerResponseSchema,
-} from "../../schemas/answer.schemas";
+import { iAnswerRequest } from "../../interfaces/answers.interfaces";
+import { answareResponseSchema } from "../../schemas/answer.schemas";
 
-export const createAnswerService = async (
-  answerBody: iAnswerRequest
-): Promise<iAnswerCreateResponse> => {
-  const userId = answerBody.userId;
-  const questionId = answerBody.questionId;
+export const createAnswerService = async (answerBody: iAnswerRequest) => {
+  const { userId, questionId } = answerBody;
 
   const answersRepository = AppDataSource.getRepository(Answer);
   const usersRepository = AppDataSource.getRepository(User);
   const questionsRepository = AppDataSource.getRepository(Question);
 
-  const question = await questionsRepository
-    .findOneByOrFail({ id: questionId })
-    .catch(() => {
-      throw new AppError("Question not found", 404);
-    });
+  const question = await questionsRepository.findOneByOrFail({ id: questionId }).catch(() => {
+    throw new AppError("Question not found", 404);
+  });
 
-  const user = await usersRepository
-    .findOneByOrFail({ id: userId })
-    .catch(() => {
-      throw new AppError("User not found", 404);
-    });
-
-  const answerValidated = await bodyAnswerSchema.validate(answerBody, {
-    stripUnknown: true,
+  const user = await usersRepository.findOneByOrFail({ id: userId }).catch(() => {
+    throw new AppError("User not found", 404);
   });
 
   const answer = answersRepository.create({
-    ...answerValidated,
+    ...answerBody,
     user,
     question,
   });
 
   await answersRepository.save(answer);
 
-  const validatedResponse = await createAnswerResponseSchema.validate(
-    { ...answer, userId, questionId },
-    {
-      stripUnknown: true,
-    }
-  );
+  const validatedResponse = await answareResponseSchema.validate(answer, {
+    stripUnknown: true,
+  });
 
   return validatedResponse;
 };
