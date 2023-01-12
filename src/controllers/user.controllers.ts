6e8bd82a061b6e08489aa435890eef3e574d2c7e
Lucas Mitori <103422552+LucasMitori@users.noreply.github.com>
2023-01-12 18:27:25 -0300

    feat/ upload Image Users completed

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import "dotenv/config";
 
 import { iUserRequest, iUserUpdate } from "../interfaces/users.interfaces";
 
@@ -7,6 +10,7 @@ import retrieveUserProfileService from "../services/users/retrieveUserProfile.se
 import listUsersService from "../services/users/listUsers.service";
 import updateUserService from "../services/users/updateUser.service";
 import deleteUserAccountService from "../services/users/deleteUserAccount.service";
+import updateImageUserService from "../services/users/uploadImageUser.service";
 
 export const registerUserController = async (
   request: Request,
@@ -40,6 +44,47 @@ export const updateUserProfileController = async (
   const userData: iUserUpdate = request.body;
   const updatedUser = await updateUserService(request.params.id, userData);
   return response.status(200).json(updatedUser);
+};
+
+export const getUploadImageController = async (
+  request: Request,
+  response: Response
+) => {
+  const { public_id } = request.params;
+  const image = cloudinary.url(public_id);
+
+  response.json(image);
+};
+
+export const uploadImageUserController = async (
+  request: Request,
+  response: Response
+) => {
+  const upload = await cloudinary.uploader.upload(
+    request.file!.path,
+    async (error, result) => {
+      const upload = await updateImageUserService(
+        result.secure_url,
+        request.params.id
+      );
+
+      const public_id = {
+        public_id: result.public_id,
+      };
+
+      const resultado = {
+        ...upload,
+        ...public_id,
+      };
+      return response.json(resultado).status(200);
+    }
+  );
+
+  fs.unlink(request.file!.path, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 };
 
 export const deleteUserAccountController = async (
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,26 @@
 import { Router } from "express";
-
+import { v2 as cloudinary } from "cloudinary";
+import "dotenv/config";
 export const usersRoutes = Router();
 
 import {
   deleteUserAccountController,
+  getUploadImageController,
   listUsersController,
   registerUserController,
   retrieveUserProfileController,
   updateUserProfileController,
+  uploadImageUserController,
 } from "../controllers/user.controllers";
 import ensureAuthMiddleware from "../middlewares/ensure.authorization.middleware";
+import { upload } from "../middlewares/upload.middleware";
+
+cloudinary.config({
+  cloud_name: "dx231szfy",
+  api_key: "118635232295459",
+  api_secret: "WO3fSBOBB1w6tXEdInyiJSf9OsM",
+  secure: true,
+});
 
 usersRoutes.post("", registerUserController);
 
@@ -20,3 +31,11 @@ usersRoutes.get("", listUsersController);
 usersRoutes.patch("/:id", ensureAuthMiddleware, updateUserProfileController);
 
 usersRoutes.delete("/:id", deleteUserAccountController);
+
+usersRoutes.post(
+  "/upload/:id",
+  upload.single("image"),
+  uploadImageUserController
+);
+
+usersRoutes.get("/upload/:id/:public_id", getUploadImageController);
