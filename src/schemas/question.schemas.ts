a4bc93ce529bbd9e6227b796800b239eb8d83939
Altieris Souza <altmsf15@gmail.com>
2023-01-12 10:32:57 -0300

    changing the typing and adding elements to the response body

--- src/schemas/question.schemas.ts
+++ src/schemas/question.schemas.ts
@@ -11,12 +11,15 @@ export const questionSchema: SchemaOf<iQuestionRequest> = yup.object().shape({
   tech: yup.string().required(),
 });
 
-export const questionReturnSchema: SchemaOf<any> = yup.object().shape({
-  user: yup.string(),
-  updatedAt: yup.date().nullable(),
-  createdAt: yup.date().nullable(),
-  deletedAt: yup.date().nullable(),
-  tech: yup.string(),
-  title: yup.string(),
-  id: yup.string(),
-});
+export const questionReturnSchema: SchemaOf<iQuestionResponse> = yup
+  .object()
+  .shape({
+    user: yup.string(),
+    updatedAt: yup.date(),
+    createdAt: yup.date(),
+    deletedAt: yup.date().nullable(),
+    tech: yup.string(),
+    description: yup.string(),
+    title: yup.string(),
+    id: yup.string(),
+  });
