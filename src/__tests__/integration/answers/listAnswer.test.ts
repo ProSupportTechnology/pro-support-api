cb3d011905696bff28c476029c57f9d907320acd
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-13 14:45:49 -0300

    feat: add connection of test in get answers

--- src/__tests__/integration/answers/listAnswer.test.ts
+++ src/__tests__/integration/answers/listAnswer.test.ts
@@ -1,3 +1,31 @@
-describe("test", () => {
-  it("test", () => {})
-})
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../../data-source";
+import request from "supertest";
+import { app } from "../../../app";
+
+describe("/answers", () => {
+  let conn: DataSource;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => {
+        conn = res;
+      })
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+
+    // await request(app).post("/users")
+  });
+
+  afterAll(async () => {
+    await conn.destroy();
+  });
+
+  it("GET /answers | Should be able to list answers", () => {
+    // const token = request(app).post("/login").send({
+    //   email: "user@mail.com",
+    //   password: "Teste1234%",
+    // });
+  });
+});
