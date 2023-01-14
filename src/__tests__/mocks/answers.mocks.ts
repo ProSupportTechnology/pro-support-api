9fa6de650bfe0664b87723fad525e49ae1e013dc
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-14 14:50:34 -0300

    feat: add mock of answer request

---
+++ src/__tests__/mocks/answers.mocks.ts
@@ -0,0 +1,7 @@
+import { iAnswerRequest } from "../../interfaces/answers.interfaces";
+
+export const mockAnswerRequest: iAnswerRequest = {
+  description: "Test description",
+  questionId: "",
+  userId: "",
+};
