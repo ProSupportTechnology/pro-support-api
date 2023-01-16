c87129562133d0c4a5f8a6e2463577d5573ff319
Wesley de Matos Souza <wesleydematos3@gmail.com>
2023-01-16 16:05:37 -0300

    fix: adjust exports from login mocks

--- src/__tests__/mocks/login.mocks.ts
+++ src/__tests__/mocks/login.mocks.ts
@@ -1,27 +1,25 @@
 import { iUserLogin, iUserRequest } from "../../interfaces/users.interfaces";
 
-const mockedAdmin: iUserRequest = {
+export const mockedAdmin: iUserRequest = {
   name: "Gaspar admin",
   email: "ghostBombado@ghost.com",
   isAdm: true,
   password: "ghost4ever",
 };
 
-const mockedUserLogin: iUserLogin = {
+export const mockedUserLogin: iUserLogin = {
   email: "Gasperboy@mail.com",
   password: "ghostnever",
 };
 
-const mockedAdminLogin: iUserLogin = {
+export const mockedAdminLogin: iUserLogin = {
   email: "ghostBombado@ghost.com",
   password: "ghost4ever",
 };
 
-const mockedUser: iUserRequest = {
+export const mockedUser: iUserRequest = {
   name: "Gasper",
   email: "Gasperboy@mail.com",
   isAdm: false,
   password: "ghostnever",
 };
-
-export { mockedAdmin, mockedAdminLogin, mockedUserLogin, mockedUser };
