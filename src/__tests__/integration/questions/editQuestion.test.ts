a361c69dc7c068801d96fd20309a2f9aef8e5634
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-17 10:12:49 -0300

    feat: change name of mock file

--- src/__tests__/integration/questions/editQuestion.test.ts
+++ src/__tests__/integration/questions/editQuestion.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { DataSource } from "typeorm";
 import { app } from "../../../app";
 import { AppDataSource } from "../../../data-source";
-import { mockedAdmin, mockedAdminLogin } from "../../mocks/login.mocks";
+import { mockedAdmin, mockedAdminLogin, mockedUser } from "../../mocks/login.mocks";
 import { mockedQuestionRequest } from "../../mocks/questions.mocks";
 
 describe("test", () => {
@@ -18,6 +18,7 @@ describe("test", () => {
       });
 
     await request(app).post("/users").send(mockedAdmin);
+    await request(app).post("/users").send(mockedUser);
   });
 
   afterAll(async () => {
@@ -30,9 +31,7 @@ describe("test", () => {
       description: "Como atribuir uma classe para as <li>?",
     };
 
-    const adminLoginResponse = await request(app)
-      .post("/login")
-      .send(mockedAdminLogin);
+    const adminLoginResponse = await request(app).post("/login").send(mockedAdminLogin);
 
     const token = `Bearer ${adminLoginResponse.body.token}`;
 
@@ -48,35 +47,41 @@ describe("test", () => {
       .set("Authorization", token)
       .send(newValues);
 
-    const questionUpdated = await request(app)
-      .get("/questions")
-      .set("Authorization", token);
+    const questionUpdated = await request(app).get("/questions").set("Authorization", token);
 
     expect(response.status).toBe(200);
     expect(questionUpdated.body[0].tech).toEqual("CSS");
-    expect(questionUpdated.body[0].description).toEqual(
-      "Como atribuir uma classe para as <li>?"
-    );
+    expect(questionUpdated.body[0].description).toEqual("Como atribuir uma classe para as <li>?");
   });
 
   it("PATCH /questions/:id -  should not be able to edit questions with id other than type uuid", async () => {
-    const adminLoginResponse = await request(app)
-      .post("/login")
-      .send(mockedAdminLogin);
+    const adminLoginResponse = await request(app).post("/login").send(mockedAdminLogin);
 
     const token = `Bearer ${adminLoginResponse.body.token}`;
 
-    const response = await request(app)
-      .patch(`/questions/13146286`)
-      .set("Authorization", `Bearer ${token}`);
+    const response = await request(app).patch(`/questions/13146286`).set("Authorization", `Bearer ${token}`);
     expect(response.status).toBe(406);
     expect(response.body).toHaveProperty("message");
   });
 
-  it("PATCH /questions/:id - Must not be able to edit a question without authentication", async () => {
+  it("PATCH /questions/:id - should not be able to update question with invalid id", async () => {
+    const newValues = { title: "Test", tech: "CSS" };
+
+    const admingLoginResponse = await request(app).post("/login").send(mockedAdminLogin);
+
+    const token = `Bearer ${admingLoginResponse.body.token}`;
+
     const response = await request(app)
-      .post("/questions")
-      .send(mockedQuestionRequest);
+      .patch(`/questions/13970660-5dbe-423a-9a9d-5c23b37943cf`)
+      .set("Authorization", token)
+      .send(newValues);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(404);
+  });
+
+  it("PATCH /questions/:id - Must not be able to edit a question without authentication", async () => {
+    const response = await request(app).post("/questions").send(mockedQuestionRequest);
 
     expect(response.body).toHaveProperty("message");
     expect(response.status).toBe(401);
